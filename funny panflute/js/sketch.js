a42a984340d392fd5a66872b4e2ebf0bf255bf13
Vera Maris <71328838+VeraMaris@users.noreply.github.com>
2020-09-22 17:17:09 +0200

    funny pan flute

---
+++ funny panflute/js/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  // put setup code here
+  createCanvas(300,300);
+  background(240);
+
+    stroke(255, 150, 0)
+    line(50, 50, 50, 250)
+
+    strokeWeight(5)
+    line(100, 50, 100, 200)
+
+    strokeWeight(1)
+    line(150, 50, 150, 150)
+
+    strokeWeight(5)
+    line(200, 50, 200, 100)
+    
+
+
+
+}
+
+function draw() {
+  // put drawing code here
+
+  //draw is 60 keer per seconde berekend het programma alles opnieuw
+  //background(mouseY,0,0);
+
+
+//random(255) --> 0 tot 255
+//random (100,110)--> 100,110. pakt willekurig nummer tussen die bepaalde getallen
+  
+}
