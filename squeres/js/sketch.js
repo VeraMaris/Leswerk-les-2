897645fa20321d36c43d59db3a5e985bd152a546
Vera Maris <71328838+VeraMaris@users.noreply.github.com>
2020-09-22 17:18:44 +0200

    Squeres!

---
+++ squeres/js/sketch.js
@@ -0,0 +1,51 @@
+function setup() {
+  // put setup code here
+  createCanvas(500,500);
+  background(240);
+
+  fill(255, 0, 0)
+  noStroke()
+  rect(50, 50, 50, 50)
+
+  fill(255, 255, 0)
+  rect(150, 50, 50, 50)
+
+  fill(0, 200, 255)
+  rect(250, 50, 50, 50)
+
+  fill(0, 255, 0)
+  rect(350, 50, 50, 50)
+
+
+  fill(255)
+  stroke(0)
+  rect(50, 200, 50, 50)
+
+  fill(200)
+  stroke(0)
+  rect(150, 200, 50, 50)
+
+  fill(100)
+  stroke(0)
+  rect(250, 200, 50, 50)
+
+  fill(50)
+  stroke(0)
+  rect(350, 200, 50, 50)
+
+
+
+
+}
+
+function draw() {
+  // put drawing code here
+
+  //draw is 60 keer per seconde berekend het programma alles opnieuw
+  //background(mouseY,0,0);
+
+
+//random(255) --> 0 tot 255
+//random (100,110)--> 100,110. pakt willekurig nummer tussen die bepaalde getallen
+  
+}
